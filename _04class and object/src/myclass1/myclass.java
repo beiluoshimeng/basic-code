78a585a62aa469425ba59450249362fca76d4f43
BLSM <admin@DESKTOP-UNC53RF>
2022-10-11 16:54:21 +0800

    增加了抽象类；开始了接口

--- _04class and object/src/myclass1/myclass.java
+++ _04class and object/src/myclass1/myclass.java
@@ -71,3 +71,42 @@ class Person {
     private void sleep() {}
     private void play() {}
 }
+
+
+/*
+* 抽象类
+* 1. 不能实例化；
+* 2. 可以有子类：通过extends关键字来继承抽象类；
+* 3. 只有在抽象类中能够定义抽象方法,抽象类应该至少有一个抽象方法，否则它没有任何意义;
+* 4. 抽象类中的抽象方法没有方法体;
+* 5. 抽象类的子类必须给出父类中的抽象方法的具体实现，除非该子类也是抽象类。
+*
+* 抽象类应用场景：
+* 1. 希望一些通用的功能被多个子类复用；
+* 2. 在抽象类中定义好 API，然后在子类中扩展实现的时候就可以使用抽象类。
+* */
+abstract class AbstractLover {
+    protected String p1;
+    protected  String p2;
+
+    protected  AbstractLover(String p1, String p2){
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+    abstract void fall_in_love();
+
+    public void friend() {
+        System.out.println("We are friend now.");
+    }
+}
+
+class Yang_aq extends AbstractLover {
+
+    protected  Yang_aq(String p1, String p2){
+        super(p1, p2);
+    }
+    @Override
+    void fall_in_love() {
+        System.out.println("We are cp now.");
+    }
+}
