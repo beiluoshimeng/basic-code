24ff0f687d35b9cd62591d49ccde2c4c117d39a7
BLSM <admin@DESKTOP-UNC53RF>
2022-11-14 15:27:37 +0800

    增加了匿名类

--- _05interface/src/myinterface1/myinterface.java
+++ _05interface/src/myinterface1/myinterface.java
@@ -20,6 +20,7 @@ public interface myinterface {
 
 
 /*
+* 接口（Interface）：抽象类型，是抽象方法的集合；通过关键字interface来定义。
 * 1. 接口中定义的所有变量或者方法，都会自动添加上 public 关键字；
 * 2. 接口中定义的变量会在编译的时候自动加上 public static final 修饰符（相当于常量）；
 * 3. 没有使用 private、default 或者 static 关键字修饰的方法是隐式抽象的，在编译的时候会自动加上 public abstract 修饰符。
@@ -30,3 +31,33 @@ public interface myinterface {
 * 6. 接口不允许直接实例化，否则编译器会报错
 * 7. 接口可以是空的
 * */
+
+
+/*
+* 抽象类与接口的区别:
+* 1. 抽象类可以有方法体的方法，但接口没有；
+* 2. 接口中的成员变量隐式为 static final，但抽象类不是的，接口不需要使用 abstract 关键字；
+* 3. 一个类可以实现多个接口，但只能继承一个抽象类。
+* */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- _06nested class/src/nested_class/nested_class.java
+++ _06nested class/src/nested_class/nested_class.java
@@ -1,9 +1,31 @@
 package nested_class;
 
+
+/*
+* 1、使用匿名内部类时，我们必须是继承一个类或者实现一个接口，但是两者不可兼得，同时也只能继承一个类或者实现一个接口。
+* 2、匿名内部类中是不能定义构造函数的。
+* 3、匿名内部类中不能存在任何的静态成员变量和静态方法。
+* 4、匿名内部类为局部内部类，所以局部内部类的所有限制同样对匿名内部类生效。
+* 5、匿名内部类不能是抽象的，它必须要实现继承的类或者实现的接口的所有抽象方法。
+* */
+
+
 public class nested_class {
     public static void main(String[] args) {
         family aq_and_y = new family();
         aq_and_y.who();
+
+        //匿名类
+        f(new thing(){
+            @Override
+            public void ml(){
+                System.out.println("pp");
+            }
+        });
+    }
+
+    public static void f(thing a){
+        a.ml();
     }
 }
 
@@ -14,9 +36,19 @@ class family{
         family aq_and_yang = new family();
         aq aq = aq_and_yang.new aq();
         aq.print();
+        hh h = new hh();
+        h.ml();
+
+        //匿名类
+        hh hhh = new hh(){
+            public void ml() {
+                System.out.println("pp");
+            }
+        };
+        hhh.ml();
     }
 
-    //成员内部类
+    //成员内部类(静态内部类不能访问外部类的非静态变量和方法)
     class aq{
         int sex = 0;
         String aq_name = "aq";
@@ -29,8 +61,17 @@ class family{
             System.out.println(name);
             System.out.println(aq_name);
             System.out.println(aq_bf.bf_name);
-
         }
     }
+}
 
+interface thing{
+    public void ml();
+}
+//不用匿名类
+class hh implements thing{
+    @Override
+    public void ml() {
+        System.out.println("pp");
+    }
 }
